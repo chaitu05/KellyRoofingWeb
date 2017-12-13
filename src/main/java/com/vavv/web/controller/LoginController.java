3c843daf156e36372b344fe68d9eba5b6477165d
kumar m <cmynam@gmail.com>
2017-12-12 17:12:21 -0700

    Sample ResponseEntity returned with headers, status, and response body.

--- src/main/java/com/vavv/web/controller/LoginController.java
+++ src/main/java/com/vavv/web/controller/LoginController.java
@@ -1,10 +1,15 @@
 package com.vavv.web.controller;
 
 import com.vavv.web.model.User;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 //import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.http.HttpStatus;
@@ -20,8 +25,35 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/signin", method = RequestMethod.POST)
-    public void login(@RequestBody User user) {
+    public ResponseEntity login(@RequestBody User user) {
         System.out.println("User: " + user);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("1", "uno");
+        return new ResponseEntity(new Object() {
+            public final boolean success = true;
+            public final Date dt = new Date();
+            public final String str = "some string";
+            public final char c = 'c';
+            public final int n = 1234;
+            public final long l = 333l;
+            public final double d = 3.3d;
+        }, headers, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/signin-anonymous-obj-return", method = RequestMethod.POST)
+    public ResponseEntity signinAnonymousObjectReturnSample(@RequestBody User user) {
+        System.out.println("User: " + user);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("1", "uno");
+        return new ResponseEntity(new Object() {
+            public final boolean success = true;
+            public final Date dt = new Date();
+            public final String str = "some string";
+            public final char c = 'c';
+            public final int n = 1234;
+            public final long l = 333l;
+            public final double d = 3.3d;
+        }, headers, HttpStatus.OK);
     }
 
     /*@RequestMapping(value = "/greeting", method = RequestMethod.GET)
